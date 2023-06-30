078428c3b708cbbef8ef42445132f7fd4a721a16
phanrahan <myemail@stanford.edu>
2023-06-30 07:06:41 -0700

    Updated nandland blink

--- nandland/blink/blink.v
+++ nandland/blink/blink.v
@@ -1,15 +1,14 @@
 // This should blink at a rate of ~ second
 
-module main (output  D5, input  CLKIN);
+module main (output  LED, input  CLK);
   
   parameter N = 24;
   
   reg [N-1:0] cnt;
   
-  always @(posedge CLKIN) begin
+  always @(posedge CLK) begin
     cnt <= cnt+1;
   end
-  assign D5 = cnt[N-1];
-
+  assign LED = cnt[N-1];
 
 endmodule
